e8dc687130c3d935709e947f4ec7a3b07e1cd7a6
Simo-Matti Siiriä <Simo-Matti.Siiria@pc.fmi.fi>
2020-08-03 12:56:51 +0300

    added logic to detect when argo has been stuck on the bottom

--- analyze_argo_raw_in_dir.py
+++ analyze_argo_raw_in_dir.py
@@ -24,8 +24,33 @@ fig_dpi = 300
 start=mp.dates.datetime.datetime(1000,5,5)
 end=mp.dates.datetime.datetime(3030,5,5)
 plot_park_pressures = True
+plot_park_pressure_deviations = False
 profiles = {}
 float_name = ""
+stuck_time = 5.0 # hours from which being stuck is calculated
+stuck_limit = 0.05  # dbar in stuck_time hours 
+def get_pressure_deviations(dat,remove_too_short = True):
+        #calculate pressure difference per minute:
+        time_d = np.array(\
+            map(lambda x: x.total_seconds(),\
+            dat['time'].diff()))
+        press_d =  dat['pressure'].diff()/time_d
+        if(remove_too_short):
+            press_d[time_d<60.0] = np.nan
+        press_d*=60.0  # to get number in minutes
+        return press_d
+
+def get_bottom_contacts(dat):
+        windowsize = dt.timedelta(hours=stuck_time/2.0)
+        bottom_contacts = [False]*dat['time'].shape[0]
+        for i in range(dat['time'].shape[0]):
+            t_window = abs(dat['time']-dat['time'][i])<windowsize
+            max_diff = abs(dat['pressure'][t_window].min() - \
+                       dat['pressure'][t_window].max())
+            if(max_diff)<stuck_limit:
+                bottom_contacts[i] = True
+        return bottom_contacts
+    
 #first things from system log
 files_to_plot=[i for i in os.listdir(dir_to_plot) if i.endswith('system_log.txt')]
 for f in files_to_plot:
@@ -66,18 +91,40 @@ for f in files_to_plot:
     dat['park_pressures']=pd.DataFrame({'pressure':park_pressure,\
                                      'time':park_times})
 
+   #PLOTTTING STARTS 
+    
 if( plot_park_pressures):
     plt.figure(figsize=(10,5))
     for prof in profiles:
         dat = profiles[prof]['park_pressures']
         tgt_pres = profiles[prof]['DeepDescentPressure']
         if(len(dat)>0):
+            contacts = get_bottom_contacts(dat)
             plt.plot(dat['time'], dat['pressure'])
             plt.plot([dat['time'].min(),dat['time'].max()],[tgt_pres,tgt_pres],'k')
             plt.fill_between(dat['time'].values, dat['pressure'],\
                              [tgt_pres]*len(dat['time'].values),
                              alpha = 0.2)
+            plt.plot(dat['time'][contacts],\
+                     dat['pressure'][contacts],\
+                        'r.',alpha = 0.05)
     plt.gca().invert_yaxis()
     plt.title(float_name)
     plt.savefig(output_dir+figure_name+'.png' ,facecolor='w',dpi=fig_dpi)
     plt.savefig(output_dir+figure_name+'.eps' ,facecolor='w',dpi=fig_dpi)
+
+if( plot_park_pressure_deviations):
+    plt.figure(figsize=(10,5))
+    for prof in profiles:
+        dat = profiles[prof]['park_pressures']
+        if(len(dat)>0):
+            #calculate pressure difference per minute:
+            press_d = get_pressure_deviations(dat, remove_too_short=False)      
+            contacts = get_bottom_contacts(dat)
+            plt.plot(dat['time'], press_d)
+            plt.plot(dat['time'][contacts], \
+                     press_d[contacts],\
+                     'r.')
+            
+    plt.gca().invert_yaxis()
+    plt.title(float_name)
