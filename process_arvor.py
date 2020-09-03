6b790abdca63f5df570635f45d189f7594c2483b
Simo-Matti Siiriä <Simo-Matti.Siiria@pc.fmi.fi>
2020-09-03 15:09:48 +0300

    changed the logic of Arvor automation a bit

--- process_arvor.py
+++ process_arvor.py
@@ -8,7 +8,7 @@ Created on Mon Aug 17 16:49:04 2020
 import os
 import shutil
 import re
-
+import datetime as dt
 process_dir = "D:\\ArvorGDrive\\ToProcess\\"
 processed_dir = "D:\\ArvorGDrive\\Archived\\"
 extract_dir = "D:\\ArvorGDrive\\Processed\\"
@@ -23,6 +23,7 @@ files_to_process = [x for x in files_to_process if x.endswith('.sbd')]
 # we want to process each file separately to avoid automtical joinings
 # and since the nke program only accepts directories, those have to be handled
 # in their own directory
+process_stamp = dt.datetime.now().strftime("%Y%m%d_%H%M")  # date and time 
 for f in files_to_process:
     shutil.move(os.path.join(process_dir,f), \
                 os.path.join(tmp_dir,f))
@@ -32,20 +33,20 @@ for f in files_to_process:
         series_number = series_number.groups()[0]
     else:
         series_number = 'xxx'
-    command = os.path.join(exec_dir,exec_name)
-    command += convert_style
-    command += tmp_dir
-    os.system(command)
-    shutil.move(os.path.join(tmp_dir,f), \
-                os.path.join(process_dir,f))
-    created_files = [x for x in os.listdir(tmp_dir) if x.endswith('.csv')]
-    #rename_created_files
-    for c_f in created_files:
-        new_name = float_name+"_"+series_number+"_"+c_f
-        shutil.move(os.path.join(tmp_dir,c_f), \
-                    os.path.join(extract_dir,new_name))
+command = os.path.join(exec_dir,exec_name)
+command += convert_style
+command += tmp_dir
+os.system(command)
+#shutil.move(os.path.join(tmp_dir,f), \
+#            os.path.join(process_dir,f))
+created_files = [x for x in os.listdir(tmp_dir) if x.endswith('.csv')]
+#rename_created_files
+for c_f in created_files:
+    new_name = float_name+"_"+process_stamp+"_"+c_f
+    shutil.move(os.path.join(tmp_dir,c_f), \
+                os.path.join(extract_dir,new_name))
         
 for f in files_to_process:
-    shutil.move(os.path.join(process_dir,f),\
+    shutil.move(os.path.join(tmp_dir,f),\
                 os.path.join(processed_dir,f))
 
