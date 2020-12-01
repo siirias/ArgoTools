136801742c12fda46edaa6719a2a21ca687baab9
Simo-Matti Siiriä <Simo-Matti.Siiria@pc.fmi.fi>
2020-12-01 19:33:59 +0200

    added new way to present the battery parameters

--- analyze_battery_consumption.py
+++ analyze_battery_consumption.py
@@ -14,40 +14,51 @@ import numpy as np
 import datetime as dt
 import matplotlib.dates as mdates
 import math
+import scipy.optimize
+import cmocean as cmo
 
 
 input_dir = "D:\\Data\\ArgoData\\ArgoRawData\\"
 out_dir = "D:\\Data\\figures\\Battery\\"
+show_fitting = False
+the_func = lambda x,a,b: (15.0-13.5)*np.exp(b*(x-a))+13.5
+
+show_trigger_voltage = True
+trigger_voltage = 13.5
+
 base_colors = [(1.0,0.0,0.0),   (0.0,1.0,0.0),  (0.0,0.0,1.0),
                (0.8,0.2,0.2),   (0.2,0.8,0.2),  (0.2,0.2,0.8),
                (0.99,0.99,0.0),   (0.99,0.0,0.99),  (0.0,0.99,0.99),
                (0.8,0.8,0.2),   (0.8,0.2,0.8),  (0.2,0.8,0.8),
-               (0.6,0.5,0.5),   (0.5,0.6,0.5),  (0.5,0.5,0.6),
+               (0.9,0.5,0.5),   (0.5,0.9,0.5),  (0.5,0.5,0.9),
                (0.4,0.2,0.2),   (0.2,0.4,0.2),  (0.2,0.4,0.2),
                (0.99,0.7,0.7),   (0.7,0.99,0.7),  (0.7,0.7,0.99)
                ]  # 21 Hand picked colors which are more or less easy to tell from eachothers
 
 float_sets = [
-        {'n':"f9234_a", 'loc':"f9234\\", 'sensors':'CTD_OB', 'wmo':'6902027'},
-        {'n':"f9234_b", 'loc':"f9234\\Old_data\\", 'sensors':'CTD_OB', 'wmo':'6902020'},
-        {'n':"f9234_c", 'loc':"f9234\\Old_data\\2013_2015\\", 'sensors':'CTD_OB', 'wmo':'6902014'},
-        {'n':"f9089_a", 'loc':"f9089\\BAPE2_2014\\", 'sensors':'CTD_OB', 'wmo':'6902018'},
-        {'n':"f9089_b", 'loc':"f9089\\BAPE2_2016\\", 'sensors':'CTD_OB', 'wmo':'6902021'},
-        {'n':"f9089_c", 'loc':"f9089\\", 'sensors':'CTD_OB', 'wmo':'6902028'},
-        {'n':"f8907", 'loc':"f8907\\", 'sensors':'CTD_O', 'wmo':'6903704'},
-        {'n':"f8543", 'loc':"f8543\\", 'sensors':'CTD_O', 'wmo':'6903700'},
-        {'n':"f8540", 'loc':"f8540\\", 'sensors':'CTD_O', 'wmo':'6903701'},
-#        {'n':"f8348", 'loc':"f8348\\", 'sensors':'CTD_O', 'wmo':''},  # Barents sea
+        {'n':"f9234_a", 'loc':"f9234\\", 'sensors':'CTD_OB', 'wmo':'6902027','area':'Baltic Proper'},
+        {'n':"f9234_b", 'loc':"f9234\\Old_data\\", 'sensors':'CTD_OB', 'wmo':'6902020','area':'Baltic Proper'},
+        {'n':"f9234_c", 'loc':"f9234\\Old_data\\2013_2015\\", 'sensors':'CTD_OB', 'wmo':'6902014','area':'Baltic Proper'},
+        {'n':"f9089_a", 'loc':"f9089\\BAPE2_2014\\", 'sensors':'CTD_OB', 'wmo':'6902018','area':'Bothnian Sea'},
+        {'n':"f9089_b", 'loc':"f9089\\BAPE2_2016\\", 'sensors':'CTD_OB', 'wmo':'6902021','area':'Bothnian Sea'},
+        {'n':"f9089_c", 'loc':"f9089\\", 'sensors':'CTD_OB', 'wmo':'6902028','area':'Bothnian Sea'},
+        {'n':"f8907", 'loc':"f8907\\", 'sensors':'CTD_O', 'wmo':'6903704','area':'N.Baltic Proper'},
+        {'n':"f8543", 'loc':"f8543\\", 'sensors':'CTD_O', 'wmo':'6903700','area':'Bay of Bothnia'},
+        {'n':"f8540", 'loc':"f8540\\", 'sensors':'CTD_O', 'wmo':'6903701','area':'Baltic Proper'},
+#        {'n':"f8348", 'loc':"f8348\\", 'sensors':'CTD_O', 'wmo':'6903695', 'area':'Barents Sea'},  # Barents sea
 #        {'n':"f8348_b", 'loc':"f8348\\iceseason18-19\\", 'sensors':'CTD_OT', 'wmo':''},
-        {'n':"f7126_a", 'loc':"f7126\\APE1_2012\\", 'sensors':'CTD', 'wmo':'6901901'},
-        {'n':"f7126_b", 'loc':"f7126\\APE1_2014-2015\\", 'sensors':'CTD', 'wmo':'6902017'},
-        {'n':"f7126_c", 'loc':"f7126\\", 'sensors':'CTD', 'wmo':'6902023'},
-        {'n':"f7087_a", 'loc':"f7087\\APE2_2013\\", 'sensors':'CTD', 'wmo':'6902013'},
-        {'n':"f7087_b", 'loc':"f7087\\APE2_2016\\", 'sensors':'CTD', 'wmo':'6902022'},
-        {'n':"f7087_c", 'loc':"f7087\\APE2_2017\\", 'sensors':'CTD', 'wmo':'6902029'},
-        {'n':"f7087_d", 'loc':"f7087\\", 'sensors':'CTD', 'wmo':'6902030'}
+        {'n':"f7126_a", 'loc':"f7126\\APE1_2012\\", 'sensors':'CTD', 'wmo':'6901901','area':'Bothnian Sea'},
+        {'n':"f7126_b", 'loc':"f7126\\APE1_2014-2015\\", 'sensors':'CTD', 'wmo':'6902017','area':'Bothnian Sea'},
+        {'n':"f7126_c", 'loc':"f7126\\", 'sensors':'CTD', 'wmo':'6902023','area':'Bothnian Sea'},
+        {'n':"f7087_a", 'loc':"f7087\\APE2_2013\\", 'sensors':'CTD', 'wmo':'6902013','area':'Bothnian Sea'},
+        {'n':"f7087_b", 'loc':"f7087\\APE2_2016\\", 'sensors':'CTD', 'wmo':'6902022','area':'Bothnian Sea'},
+        {'n':"f7087_c", 'loc':"f7087\\APE2_2017\\", 'sensors':'CTD', 'wmo':'6902029','area':'Bothnian Sea'},
+        {'n':"f7087_d", 'loc':"f7087\\", 'sensors':'CTD', 'wmo':'6902030','area':'Bothnian Sea'}
         
         ]
+
+print("WMO;Sensors;Software;Start;End;Profiles;Groundings;avg.Depth;Avg.Control a;Area")
+
 conversion_to_new_piston_step = 16  # New APX11 software has finer steps,
                                     # as such old steps must be multiplied.
 #float_sets = [
@@ -109,7 +120,6 @@ def bottom_contact(log_lines, float_type = 'apex11'):
     return contact_found
 
 
-print("WMO;Sensors;Software;Start;End;Profiles;Groundings;avg.Depth;Avg.Control actions")
 for f_s in float_sets:
     current_input_dir = input_dir+f_s['loc']
     f_s['type'] = "apex9"
@@ -239,62 +249,71 @@ for f_s in float_sets:
                     total_control_steps.append(tmp_control_steps+total_control_steps[-1])
                 control_steps.append(tmp_control_steps)
                 
-        f_s['voltages'] = voltages.copy()
-        f_s['cycles'] = cycles.copy()
+        f_s['voltages'] = np.array(voltages)
+        f_s['cycles'] = np.array(cycles)
         try:
-            f_s['lifetime'] = list(map(lambda x: (x-dates[0]).days,dates))
+            f_s['lifetime'] = np.array(list(map(lambda x: (x-dates[0]).days,dates)))
             f_s['dates'] = dates.copy()
         except:
-            f_s['lifetime'] = list(map(lambda x: None,dates))
+            f_s['lifetime'] = np.array(list(map(lambda x: None,dates)))
             f_s['dates'] = f_s['lifetime'].copy()
-        f_s['travelled_depth'] = travelled_depth.copy()
-        f_s['profile_depths'] = profile_depths.copy()
-        f_s['total_control_steps'] = total_control_steps.copy()
-        f_s['control_steps'] = control_steps.copy()
-        f_s['total_control_actions'] = total_control_actions.copy()
-        f_s['control_actions'] = control_actions.copy()
-        f_s['bottom_contacts'] = bottom_contacts
-#    print("From {} to {}".format(f_s['dates'][0], f_s['dates'][-1]))
-#    print("distance {} m".format(f_s['travelled_depth'][-1]))
-    print("{};{};{};{};{};{};{:.1f};{:.1f};{:.1f}".format(\
+        f_s['travelled_depth'] = np.array(travelled_depth)
+        f_s['profile_depths'] = np.array(profile_depths)
+        f_s['total_control_steps'] = np.array(total_control_steps)
+        f_s['control_steps'] = np.array(control_steps)
+        f_s['total_control_actions'] = np.array(total_control_actions)
+        f_s['control_actions'] = np.array(control_actions)
+        f_s['bottom_contacts'] = np.array(bottom_contacts)
+    f_s['trigger_index'] = np.argmin(np.abs(f_s['voltages']-trigger_voltage))
+    print("{};{};{};{};{};{};{:.1f};{:.1f};{:.1f};{}".format(\
           f_s['wmo'], f_s['sensors'], f_s['type'],\
           dt.datetime.strftime(f_s['dates'][0],'%Y-%m-%d'),\
           dt.datetime.strftime(f_s['dates'][-1],'%Y-%m-%d'),\
           len(f_s['dates']),\
           f_s['bottom_contacts'],\
-          np.mean(np.array(f_s['profile_depths'])),\
-          float(f_s['total_control_actions'][-1])/len(f_s['dates'])))
+          np.mean(f_s['profile_depths']),\
+          float(f_s['total_control_actions'][-1])/len(f_s['dates']),\
+            f_s['area']))
+
 for i in float_sets:
-    i['voltage_perc'] = np.array(i['voltages'])/np.array(i['voltages']).max()
-        
+    i['voltages_perc'] = i['voltages']/i['voltages'].max()
+    i['fit']= {}
+    for xfield in ['travelled_depth', 'lifetime', 'cycles', 'total_control_steps', 'total_control_actions']:
+        limit = i['voltages']>13.5
+        fitted, covar = scipy.optimize.curve_fit(\
+            the_func, \
+            i[xfield][limit], i['voltages'][limit],\
+            [0.0, -0.001],\
+            bounds = ([-np.inf, -0.5],[np.inf, -0.0000001]), maxfev = 10000)
+        i['fit'][xfield] = fitted
 
     
-#figure_types = [
-#        {'title':'Voltage per depth distance',
-#         'xlabel':'Travelled depth',
-#        'ylabel':'Voltage(fraction of maximum)',
-#        'xfield':'travelled_depth',
-#        'yfield':'voltages_perc'},
-#
-#        {'title':'Voltage per time',
-#         'xlabel':'Mission time(days)',
-#        'ylabel':'Voltage(fraction of maximum)',
-#        'xfield':'lifetime',
-#        'yfield':'voltages_perc'},
-#         
-#        {'title':'Voltage per cycles',
-#         'xlabel':'Cycle no',
-#        'ylabel':'Voltage(fraction of maximum)',
-#        'xfield':'cycles',
-#        'yfield':'voltages_perc'},
-#
-#        {'title':'Voltage per Control steps',
-#         'xlabel':'Piston movement steps',
-#        'ylabel':'Voltage(fraction of maximum)',
-#        'xfield':'total_control_steps',
-#        'yfield':'voltages_perc'}
-#
-#        ]
+figure_types = [
+        {'title':'PercVoltage per depth distance',
+         'xlabel':'Travelled depth',
+        'ylabel':'Voltage(fraction of maximum)',
+        'xfield':'travelled_depth',
+        'yfield':'voltages_perc'},
+
+        {'title':'PercVoltage per time',
+         'xlabel':'Mission time(days)',
+        'ylabel':'Voltage(fraction of maximum)',
+        'xfield':'lifetime',
+        'yfield':'voltages_perc'},
+         
+        {'title':'PercVoltage per cycles',
+         'xlabel':'Cycle no',
+        'ylabel':'Voltage(fraction of maximum)',
+        'xfield':'cycles',
+        'yfield':'voltages_perc'},
+
+        {'title':'PercVoltage per Control steps',
+         'xlabel':'Piston movement steps',
+        'ylabel':'Voltage(fraction of maximum)',
+        'xfield':'total_control_steps',
+        'yfield':'voltages_perc'},
+
+        ]
 
 figure_types = [
         {'title':'Voltage per depth distance',
@@ -319,6 +338,12 @@ figure_types = [
          'xlabel':'Piston movement steps',
         'ylabel':'Voltage (V)',
         'xfield':'total_control_steps',
+        'yfield':'voltages'},
+
+        {'title':'Voltage per Control actions',
+         'xlabel':'Piston movement actions',
+        'ylabel':'Voltage (V)',
+        'xfield':'total_control_actions',
         'yfield':'voltages'}
 
         ]
@@ -326,8 +351,13 @@ figure_types = [
 
 #color_scheme = 'by_bottom_contacts'
 #color_scheme = 'by_average_profile_depth'
-color_scheme = 'by_average_profile_time'
+#color_scheme = 'by_average_profile_time'
 color_scheme = 'none'
+
+#mark_scheme = 'none'
+#mark_scheme = 'area'
+mark_scheme = 'sensors'
+
 max_profile_depth_average = 0.0
 max_profile_time_average = 0.0
 # figure out maximum profile depths etc.
@@ -340,6 +370,7 @@ for i in float_sets:
         max_profile_time_average = tmp
         
 for ft in figure_types:
+    print(ft['title'])
     plt.figure(figsize=(12,8))
     next_color = 0        
     for i in float_sets:
@@ -356,23 +387,182 @@ for ft in figure_types:
             bc_f = np.array(i['profile_depths']).mean()/max_profile_depth_average
             set_color = (bc_f, 0.5, 1.0 - bc_f)
         if(color_scheme == 'by_average_profile_time'):
-            bc_f = i['lifetime'][-1]/i['cycles'][-1]/max_profile_time_average
+            bc_f = (float(i['lifetime'][-1])/float(i['cycles'][-1]))/max_profile_time_average
             set_color = (0.5, bc_f, 1.0 - bc_f)
-            
+        the_mark = None
+        if(mark_scheme == 'area'):
+            if( i['area'].lower() == 'baltic proper'):
+                the_mark = 'x'
+            if( i['area'].lower() == 'bothnian sea'):
+                the_mark = '.'
+            if( i['area'].lower() == 'bay of bothnia'):
+                the_mark = 's'
+            if( i['area'].lower() == 'n.baltic proper'):
+                the_mark = 11
+        if(mark_scheme == 'sensors'):
+            if( i['sensors'].lower() == 'ctd'):
+                the_mark = 'x'
+            if( i['sensors'].lower() == 'ctd_o'):
+                the_mark = '.'
+            if( i['sensors'].lower() == 'ctd_ob'):
+                the_mark = 'o'
+                
         if('voltages' in i.keys()):
 #            the_label = "{} bc:{:.1f} %".format(i['wmo'],100.0*i['bottom_contacts']/i['cycles'][-1])
             the_label = "{}".format(i['wmo'])
-            plt.plot(np.array(i[ft['xfield']][:]),\
+            plt.plot(i[ft['xfield']][:],\
                               i[ft['yfield']][:], \
                               label = the_label,\
-                              linewidth = lw, color = set_color)
+                              linewidth = lw, color = set_color, marker = the_mark)
+            plt.grid(alpha= 0.25)
+            if(show_fitting):
+                limit = i['voltages']>13.5
+                print(i['fit'][ft['xfield']])
+                plt.plot(i[ft['xfield']][limit],\
+                                  the_func(np.array(i[ft['xfield']][limit]),\
+                                  i['fit'][ft['xfield']][0],\
+                                  i['fit'][ft['xfield']][1]), \
+                                  linewidth = lw, color = set_color,\
+                                  alpha = 0.25, marker = the_mark )
+#            if(show_trigger_voltage):
+#                plt.axvline(i[ft['xfield']][i['trigger_index']],\
+#                            color = set_color, alpha = 0.25)
         else:
             print("Different format: {}".format(i['n']))
-        plt.legend()
-        plt.ylabel(ft['ylabel'])
-        plt.xlabel(ft['xlabel'])
-        plt.title(ft['title'])
-        out_filename = "{}.jpg".format(re.sub(" ","_",ft['title']))
-        plt.savefig(out_dir+out_filename,\
-                            facecolor='w', dpi=300, bbox_inches='tight')
+    if(show_trigger_voltage):
+        plt.axhline(trigger_voltage,color = (0.5,0.5,0.5))
+    plt.legend()
+    plt.ylabel(ft['ylabel'])
+    plt.xlabel(ft['xlabel'])
+    plt.title(ft['title'])
+    out_filename = "{}.jpg".format(re.sub(" ","_",ft['title']))
+    plt.savefig(out_dir+out_filename,\
+                        facecolor='w', dpi=300, bbox_inches='tight')
 
+#print table with coefficients, and calulate some derived variables:
+print("WMO;Decay;avg_depth;col/prof;day/prof;cntrolstep/prof")
+for i in float_sets:
+    i['decay'] = i['fit']['lifetime'][1]
+    i['mean_profile_depth'] = i['profile_depths'].mean()
+    i['contact_fraction'] = float(i['bottom_contacts'])/len(i['profile_depths'])
+    i['mean_days_per_profile'] = i['lifetime'][-1]/float(len(i['lifetime']))
+    i['mean_control_step_per_profile'] = float(i['total_control_steps'][-1])/len(i['cycles'])
+    i['mean_control_actions_per_profile'] = float(i['total_control_actions'][-1])/len(i['cycles'])
+
+    print("{};{:.3f};{:.1f};{:.2f};{:.1f};{:.1f}".format(\
+          i['wmo'],
+          i['decay'], i['mean_profile_depth'],\
+          i['contact_fraction'], i['mean_days_per_profile'],\
+          i['mean_control_step_per_profile'], \
+          i['mean_control_actions_per_profile']))        
+
+
+scatter_plot_types = [
+    {'title':'Mission days (until Vt) depth_days',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average days between profiles',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_days_per_profile',
+     'zfield':'lifetime',
+     'zlabel':'Days before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.thermal},
+
+    {'title':'Mission days (until Vt) depth_steps',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average control steps per profile',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_control_step_per_profile',
+     'zfield':'lifetime',
+     'zlabel':'Days before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.thermal},
+
+    {'title':'Mission days (until Vt) depth_contacts',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Fraction of bottom contacts',
+     'xfield':'mean_profile_depth',
+     'yfield':'contact_fraction',
+     'zfield':'lifetime',
+     'zlabel':'Days before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.thermal},
+
+    {'title':'Profiles (until Vt) depth_days',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average days between profiles',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_days_per_profile',
+     'zfield':'cycles',
+     'zlabel':'Profiles before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.solar},
+
+    {'title':'Profiles (until Vt) depth_steps',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average control steps per profile',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_control_step_per_profile',
+     'zfield':'cycles',
+     'zlabel':'Profiles before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.solar},
+
+    {'title':'Profiles (until Vt) depth_contacts',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Fraction of bottom contacts',
+     'xfield':'mean_profile_depth',
+     'yfield':'contact_fraction',
+     'zfield':'cycles',
+     'zlabel':'Profiles before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.solar},
+
+    {'title':'Vertical distance (until Vt) depth_days',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average days between profiles',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_days_per_profile',
+     'zfield':'travelled_depth',
+     'zlabel':'Meters dived before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.haline},
+
+    {'title':'Vertical distance (until Vt) depth_steps',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Average control steps per profile',
+     'xfield':'mean_profile_depth',
+     'yfield':'mean_control_step_per_profile',
+     'zfield':'travelled_depth',
+     'zlabel':'Meters dived before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.haline},
+
+    {'title':'Vertical distance (until Vt) depth_contacts',
+     'xlabel':'Average profile depth (m)',
+     'ylabel':'Fraction of bottom contacts',
+     'xfield':'mean_profile_depth',
+     'yfield':'contact_fraction',
+     'zfield':'travelled_depth',
+     'zlabel':'Meters dived before trigger voltage ({})'.format(trigger_voltage),
+     'cmap':cmo.cm.haline},
+
+     ]
+for ft in scatter_plot_types:
+    plt.figure(figsize=(6,5))
+    lim_min = float_sets[0][ft['zfield']][i['trigger_index']]
+    lim_max = lim_min
+    cmap = ft['cmap']
+    for i in float_sets:
+        if(i[ft['zfield']][i['trigger_index']]>lim_max):
+            lim_max = i[ft['zfield']][i['trigger_index']]
+        if(i[ft['zfield']][i['trigger_index']]<lim_min):
+            lim_min = i[ft['zfield']][i['trigger_index']]
+    for i in float_sets:  # mission time as a function of frequency, aveverage depth
+        x = i[ft['xfield']]
+        y = i[ft['yfield']]
+        the_color = cmap((i[ft['zfield']][i['trigger_index']]-lim_min)/(lim_max-lim_min))
+        plt.plot(x,y,marker = 'o', markersize = 10, color = the_color)
+        plt.text(x,y,"{}\n{}\n".format(i['wmo'],i['area']), \
+                 horizontalalignment = 'center', fontsize = 6, alpha = 0.5)
+    plt.grid(alpha=0.25)
+    plt.title(ft['title'])
+    plt.xlabel(ft['xlabel'])
+    plt.ylabel(ft['ylabel'])
+    cb = plt.colorbar(plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin=lim_min, vmax=lim_max)))
+    cb.set_label(ft['zlabel'])
+    out_filename = "{}.jpg".format(re.sub(" ","_",ft['title']))
+    plt.savefig(out_dir+out_filename,\
+                        facecolor='w', dpi=300, bbox_inches='tight')
