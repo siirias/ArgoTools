bed063a661f1e2e5ecc6b15018e7da1d4befa611
siirias <simo.siiria@fmi.fi>
2022-08-29 18:06:55 +0300

    added small script to plot shallowest depth analysis

---
+++ plots_for_D29.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 29 17:36:45 2022
+
+@author: siirias
+Extra Plots for deliverable.
+"""
+
+import datetime as dt
+import matplotlib as mp
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from matplotlib import colors as mcolors
+import xarray as xr
+
+output_dir = "C:/Data/ArgoData/Figures/"
+fig_dpi = 300
+
+WMOs = [6903709, 6903710]
+dirs = ["C:/Data/ArgoData/ArgosForPlot/RBR_BalticProper/",
+           "C:/Data/ArgoData/ArgosForPlot/RBR_BothnianSea/"]
+for WMO, indir in zip(WMOs, dirs):
+    d_arvor = xr.open_dataset(indir + 
+        "/GL_PR_PF_{}.nc".format(
+            WMO))
+    plt.figure(figsize=(8,4));
+    plt.plot(d_arvor['TIME'][:], d_arvor['PRES'][:,0],'.')
+    plt.xlabel('Time')
+    plt.ylabel('Minimum pressure of the profile (dbar)')
+    plt.grid('on')
+    plt.title('WMO {}, Baltic Proper'.format(WMO))
+    
+    filename = "{}_{}".format("Minimum_pressures",WMO)
+    plt.savefig(output_dir+filename+'.png' ,\
+            facecolor='w',dpi=fig_dpi,bbox_inches='tight')
+    print("saved {}".format(output_dir+filename+'.png'))
+    
+    
